73dd6bf7a194bc7a4e1463bc3c6a2af4f47e5215
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-10-18 22:29:37 +0900

    エロクアント作成

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
+
+        // Eloquent(エロクアント)
         $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+
         return view('tests.test', compact('values'));
     }
 }
